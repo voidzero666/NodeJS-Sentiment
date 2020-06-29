59111a2cf1c73a6b75ab8f3089febe5bad47a3ba
Marco van Poortvliet <marc0tjevp@gmail.com>
2020-06-29 15:30:39 +0200

    Data sets

--- analysis/sentiment.js
+++ analysis/sentiment.js
@@ -1,83 +1,78 @@
-const afinnJson = require ("../lists/afinn.json");
+const afinnJson = require("../lists/afinn.json");
 
 // tokenize input string and replace characters with regex
 const tokenizeInput = (input) => {
-    return input
-        .toLowerCase()
-        .replace(/\n/g, ' ')
-        .replace(/[.,\/#!?$%\^&\*;:{}=_`\"~()]/g, ' ')
-        .replace(/\s\s+/g, ' ')
-        .trim()
-        .split(' ');
-}
+  return input
+    .toLowerCase()
+    .replace(/\n/g, " ")
+    .replace(/[.,\/#!?$%\^&\*;:{}=_`\"~()]/g, " ")
+    .replace(/\s\s+/g, " ")
+    .trim()
+    .split(" ");
+};
 
 //bereken de negatieve waarde adhv. tokens uit afinn wordlist
-function calcNegative (input) {
-    var addPush = function (token, score){
-        hits -= score;
-        words.push(token)
+function calcNegative(input) {
+  var addPush = function (token, score) {
+    hits -= score;
+    words.push(token);
+  };
+
+  var tokens = tokenizeInput(input),
+    hits = 0,
+    words = [];
+
+  tokens.forEach(function (token) {
+    if (afinnJson.hasOwnProperty(token)) {
+      if (afinnJson[token] < 0) {
+        addPush(token, afinnJson[token]);
+      }
     }
+  });
 
-    var tokens = tokenizeInput(input),
-    hits   = 0,
-    words  = [];
-
-    tokens.forEach(function(token) {
-        if (afinnJson.hasOwnProperty(token)) {
-          if (afinnJson[token] < 0){
-            addPush(token, afinnJson[token]);
-          }
-        }
-      });
-
-      return {
-        score       : hits,
-        comparative : hits / tokens.length,
-        words       : words
-      };
-
+  return {
+    score: hits,
+    comparative: hits / tokens.length,
+    words: words,
+  };
 }
 
-//bereken de positieve waarde adhv. tokens uit afinn wordlist
-function calcPositive (input) {
-    var addPush = function (token, score){
-        hits += score;
-        words.push(token)
+// Bereken de positieve waarde adhv. tokens uit afinn wordlist
+function calcPositive(input) {
+  var addPush = function (token, score) {
+    hits += score;
+    words.push(token);
+  };
+
+  var tokens = tokenizeInput(input),
+    hits = 0,
+    words = [];
+
+  tokens.forEach(function (token) {
+    if (afinnJson.hasOwnProperty(token)) {
+      if (afinnJson[token] > 0) {
+        addPush(token, afinnJson[token]);
+      }
     }
+  });
 
-    var tokens = tokenizeInput(input),
-    hits   = 0,
-    words  = [];
-
-    tokens.forEach(function(token) {
-        if (afinnJson.hasOwnProperty(token)) {
-          if (afinnJson[token] > 0){
-            addPush(token, afinnJson[token]);
-          }
-        }
-      });
-
-      return {
-        score       : hits,
-        comparative : hits / tokens.length,
-        words       : words
-      };
-
+  return {
+    score: hits,
+    comparative: hits / tokens.length,
+    words: words,
+  };
 }
 
 const sent_analysis = (input) => {
+  var negative = calcNegative(input);
+  var positive = calcPositive(input);
 
-    var negative = calcNegative(input);
-    var positive = calcPositive(input);
-
-    return {
-        score : positive.score - negative.score,
-        comparative : positive.comparative - negative.comparative,
-        positive,
-        negative,
-    };
-
-}
-
+  return {
+    score: positive.score - negative.score,
+    comparative: positive.comparative - negative.comparative,
+    positive,
+    negative,
+  };
+};
 
 module.exports = sent_analysis;
--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 const analyse = require("./analysis/sentiment");
 
+const appleData = require("./lists/apple_tweets.json");
+const elonData = require("./lists/elonmusk_tweets.json");
+const trumpData = require("./lists/trump_tweets.json");
 
-var result = analyse('');
+let appleTweets = "";
+let trumpTweets = "";
+let elonTweets = "";
 
+appleData.map((tweet) => {
+  appleTweets += tweet.text;
+})
+
+trumpData.map((tweet) => {
+  trumpTweets += tweet.Tweet;
+});
+
+elonData.map((tweet) => {
+  elonTweets += tweet.text;
+});
+
+var result = analyse(appleTweets);
 console.dir(result);
